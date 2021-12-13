c284a0437bd20cdf465252aea4dce20aa296f46d
del <kingston.coker@statista.com>
2021-12-13 16:02:00 +0100

    yaml and log configurations
    Issue:
    Failed to configure a DataSource: 'url' attribute is not specified and no embedded datasource could be configured.
    
    Resolution:
    (exclude = {DataSourceAutoConfiguration.class})

--- user-data/src/main/java/com/deloop/user/data/UserDataApplication.java
+++ user-data/src/main/java/com/deloop/user/data/UserDataApplication.java
@@ -2,8 +2,9 @@ package com.deloop.user.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class} /* purpose of : (exclude = {DataSourceAutoConfiguration.class})*/)
 public class UserDataApplication {
 
     public static void main(String[] args) {
