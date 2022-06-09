332aeb833be001aea12cfb9ce0057af81dfcf7e3
Del <kingston.coker@statista.com>
2022-06-09 23:28:01 +0200

    Merge branch 'main' into develop

--- user-data/src/main/java/com/deloop/user/data/api/dtos/ProviderAccountDto.java
+++ user-data/src/main/java/com/deloop/user/data/api/dtos/ProviderAccountDto.java
@@ -2,6 +2,52 @@ package com.deloop.user.data.api.dtos;
 
 
 import java.io.Serializable;
+import java.util.Objects;
 
-public record ProviderAccountDto(long id, String provider, String profileLink) implements Serializable {
+public final class ProviderAccountDto implements Serializable {
+    private final long id;
+    private final String provider;
+    private final String profileLink;
+
+    public ProviderAccountDto(long id, String provider, String profileLink) {
+        this.id = id;
+        this.provider = provider;
+        this.profileLink = profileLink;
+    }
+
+    public long id() {
+        return id;
+    }
+
+    public String provider() {
+        return provider;
+    }
+
+    public String profileLink() {
+        return profileLink;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        ProviderAccountDto that = (ProviderAccountDto) obj;
+        return this.id == that.id &&
+                Objects.equals(this.provider, that.provider) &&
+                Objects.equals(this.profileLink, that.profileLink);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, provider, profileLink);
+    }
+
+    @Override
+    public String toString() {
+        return "ProviderAccountDto[" +
+                "id=" + id + ", " +
+                "provider=" + provider + ", " +
+                "profileLink=" + profileLink + ']';
+    }
+
 }
